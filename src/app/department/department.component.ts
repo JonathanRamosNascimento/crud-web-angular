1dfb1fd227f3dede157171ecdcab94efdc9b725a
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-08 16:29:23 -0300

    Creating the department service and making component changes to list them in the interface

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,3 +1,4 @@
+import { DepartmentService } from './../department.service';
 import { Department } from './../department';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,19 +11,34 @@ export class DepartmentComponent implements OnInit {
 
   depName: string = '';
   departments: Department[] = [
-    {name: 'dep 1', _id: 'sdiaoj2iodjasoi'},
-    {name: 'dep 2', _id: 'oj2iodsdasfjasoi'},
-    {name: 'dep 3', _id: 'fdasdf3et'},
-    {name: 'dep 4', _id: 'dgasdgwet3'},
+    { name: 'dep 1', _id: 'sdiaoj2iodjasoi' },
+    { name: 'dep 2', _id: 'oj2iodsdasfjasoi' },
+    { name: 'dep 3', _id: 'fdasdf3et' },
+    { name: 'dep 4', _id: 'dgasdgwet3' },
   ];
 
-  constructor() { }
+  constructor(
+    private departmentService: DepartmentService
+  ) { }
 
   ngOnInit() {
+    this.departmentService.get().subscribe((deps) => this.departments = deps);
   }
 
   save() {
+    this.departmentService.add({ name: this.depName })
+      .subscribe(
+        (dep) => {
+          console.log(dep);
+          this.clearFields();
+        },
+        (err) => {
+          console.error(err);
+        })
+  }
 
+  clearFields() {
+    this.depName = '';
   }
 
   cancel() {
